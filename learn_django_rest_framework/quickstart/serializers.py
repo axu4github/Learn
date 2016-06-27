eb955f915fae27f951a3d4a030446fd72aeaa285
anxu <axu.home@gmail.com>
2016-06-27 10:44:23 +0800

    实现添加最基本的Serialization(serializers.Serializer)，并使用在Views中

---
+++ learn_django_rest_framework/quickstart/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
+
+
+class QuerySerializer(serializers.Serializer):
+    sql = serializers.CharField(
+        required=True, allow_blank=False, max_length=100)
