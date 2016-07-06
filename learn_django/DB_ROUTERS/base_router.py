700c4d3bdb62fabeb207d2ea73caab198355a8df
anxu <axu.home@gmail.com>
2016-07-06 16:15:33 +0800

    提交目录改名使用git add --all

--- learn_django/DB_ROUTERS/base_router.py
+++ learn_django/DB_ROUTERS/base_router.py
@@ -1,19 +1,9 @@
+
 class BaseRouter(object):
 
     def db_for_read(self, model, **hints):
+        
+        if not model._meta.managed :
+            return 'metadata'
+
         return None
-
-    def db_for_write(self, model, **hints):
-        return None
-
-    def allow_relation(self, obj1, obj2, **hints):
-        return None
-
-    def allow_migrate(self, db, app_label, model=None, **hints):
-        try:
-            if model._meta.db_tablespace :
-                return db == model._meta.db_tablespace
-        except Exception, e:
-            pass
-
-        return db == 'default'
