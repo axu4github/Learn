5757623127794cd206f4707bea4b025678b28d4a
anxu <axu.home@gmail.com>
2016-07-25 15:00:45 +0800

    测试析构函数是否可以获取异常

--- learn_python/test_class_del_func.py
+++ learn_python/test_class_del_func.py
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8 -*-
 '''
-测试当调用类方法返回值后，类析构函数修改返回值内容是否有效
+- 测试当调用类方法返回值后，类析构函数修改返回值内容是否有效：有效
+- 测试析构函数是否可以获取到已返回函数中修改的值：可以
+- 测试析构函数是否可以获取异常：不可以
 
 '''
 
@@ -12,14 +14,19 @@ class Test(object):
         self.arg = {'a': 'a'}
 
     def __del__(self):
-        print "this is test del func"
-        self.arg.update({'b': 'b'})
-        print "--del--"
-        print self.arg
-        print "--del--"
+        try:
+            print "this is test del func"
+            self.arg.update({'b': 'b'})
+            print "--del--"
+            print self.arg
+            print "--del--"
+        except Exception, e:
+            print "--- is exception ---"
+            print e
 
     def run(self):
         self.arg.update({'c': 'c'})
+        raise Exception("123123")
         print "this is run func"
         return self.arg
 
