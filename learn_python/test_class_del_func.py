c1097b8954b82710d9d6e3514a45e4f34ef1e014
anxu <axu.home@gmail.com>
2016-07-25 14:49:02 +0800

    测试当调用类方法返回值后，类析构函数修改返回值内容是否有效

---
+++ learn_python/test_class_del_func.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+'''
+测试当调用类方法返回值后，类析构函数修改返回值内容是否有效
+
+'''
+
+
+class Test(object):
+    """docstring for Test"""
+
+    def __init__(self):
+        self.arg = {'a': 'a'}
+
+    def __del__(self):
+        print "this is test del func"
+        self.arg.update({'b': 'b'})
+
+    def run(self):
+        self.arg.update({'c': 'c'})
+        print "this is run func"
+        return self.arg
+
+if __name__ == '__main__':
+    print Test().run()
