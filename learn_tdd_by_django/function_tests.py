389651635031ab91e93fc17ed560c1dead0f8050
anxu <axu.home@gmail.com>
2016-08-04 16:17:48 +0800

    更新功能测试

--- learn_tdd_by_django/function_tests.py
+++ learn_tdd_by_django/function_tests.py
@@ -4,7 +4,9 @@
 
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -39,23 +41,41 @@ class NewVisitorTest(unittest.TestCase):
 
         # 她按回车键后,页面更新了
         # 待办事项表格中显示了“1: Buy peacock feathers”
-        # inputbox.send_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows))
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table -- its text was:\n%s" % (
+                table.text,
+            )
+        )
+
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # 页面中又显示了一个文本框,可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly(”使用孔雀羽毛做假蝇)
         # 伊迪丝做事很有条理
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
-        # 页面再次更新,她的清单中显示了这两个待办事项 # 伊迪丝想知道这个网站是否会记住她的清单
+        # 页面再次更新,她的清单中显示了这两个待办事项 
+        # 伊迪丝想知道这个网站是否会记住她的清单
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
 
         # 她看到网站为她生成了一个唯一的URL
         # 而且页面中有一些文字解说这个功能
         # 她访问那个URL,发现她的待办事项列表还在
         # 她很满意,去睡觉了
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
 
--- learn_tdd_by_django/lists/views.py
+++ learn_tdd_by_django/lists/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from django.views.decorators.csrf import csrf_protect
+
 # home_page = None
 
-
+# @csrf_protect
 def home_page(request):
     # return HttpResponse('<html><title>To-Do lists</title></html>')
-    return render(request, 'home.html')
+
+    # if request.method == 'POST':
+    #     return HttpResponse(request.POST['item_text'])
+    # return render(request, 'home.html')
+
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
