cfceff37f0acc9512277eac8ccf8f79fbd3a5ed6
anxu <axu.home@gmail.com>
2016-08-04 16:28:04 +0800

    重构功能测试代码

--- learn_tdd_by_django/function_tests.py
+++ learn_tdd_by_django/function_tests.py
@@ -18,6 +18,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # 伊迪丝听说有一个很酷的在线待办事项应用
@@ -43,33 +48,38 @@ class NewVisitorTest(unittest.TestCase):
         # 待办事项表格中显示了“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table -- its text was:\n%s" % (
-                table.text,
-            )
-        )
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     "New to-do item did not appear in table -- its text was:\n%s" % (
+        #         table.text,
+        #     )
+        # )
 
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        # self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面中又显示了一个文本框,可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly(”使用孔雀羽毛做假蝇)
         # 伊迪丝做事很有条理
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
+
         inputbox.send_keys(Keys.ENTER)
 
-        # 页面再次更新,她的清单中显示了这两个待办事项 
+        # 页面再次更新,她的清单中显示了这两个待办事项
         # 伊迪丝想知道这个网站是否会记住她的清单
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        # self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        # self.assertIn(
+        #     '2: Use peacock feathers to make a fly',
+        #     [row.text for row in rows]
+        # )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
 
         # 她看到网站为她生成了一个唯一的URL
         # 而且页面中有一些文字解说这个功能
