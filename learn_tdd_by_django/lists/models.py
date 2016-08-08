6ddaf48beac6ccb62ecba984c701d34f78bfb958
anxu <axu.home@gmail.com>
2016-08-08 13:48:36 +0800

    新增List模型并优化程序

--- learn_tdd_by_django/lists/models.py
+++ learn_tdd_by_django/lists/models.py
@@ -3,5 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
--- learn_tdd_by_django/lists/views.py
+++ learn_tdd_by_django/lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
-from lists.models import Item
+from lists.models import Item, List
 
 # home_page = None
 
@@ -18,5 +18,6 @@ def view_list(request):
 
 @csrf_exempt
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
