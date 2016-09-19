dbc4f9a6a1c3dd5cc5c158c70e12effbad29e770
axu4github <axu@axu4iMac.local>
2016-09-19 22:23:32 +0800

    修改学习scala程序

--- learn_scala_with_sbt/hello.scala
+++ learn_scala_with_sbt/hello.scala
@@ -1,3 +1,9 @@
-object Hi {
-  def main(args: Array[String]) = println("Hi!")
+class HelloWorld {
+    println("this is class.")
+}
+
+object HelloWorld {
+    def main(args: Array[String]): Unit = {
+        println("Hello World!")
+    }
 }
