bf7233af9767c5c3e79decb17295ae5224272067
axu4github <axu@axu4iMac.local>
2016-09-20 22:26:04 +0800

    新增打印传入参数程序

--- learn_scala_with_sbt/hello.scala
+++ learn_scala_with_sbt/hello.scala
@@ -1,9 +1,10 @@
-class HelloWorld {
-    println("this is class.")
-}
 
 object HelloWorld {
     def main(args: Array[String]): Unit = {
         println("Hello World!")
+
+        for (arg: String <- args) {
+            println(arg)
+        }
     }
 }
