6f6fbb2f12a8e59b2a506fb2f8561f0a9985c9f9
axu4github <axu@axu4iMac.local>
2016-10-31 23:58:43 +0800

    添加解释GetOrElseUpdate方法

---
+++ learn_scala_with_sbt/04.hashMapGetOrElseUpdateFunc.scala
@@ -0,0 +1,10 @@
+/**
+ * 了解 scala.collection.mutable.HashMap 的 getOrElseUpdate方法
+ * 
+ */
+
+object main {
+    def main(args: Array[String]): Unit = {
+        var hm = new scala.collection.mutable.HashMap
+    }
+}
