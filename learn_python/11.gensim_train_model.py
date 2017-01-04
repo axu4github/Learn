087c45d2d079bc89376873a790cd27d417ce5e90
anxu <axu.home@gmail.com>
2017-01-04 16:56:32 +0800

    设置min_count=1

--- learn_python/11.gensim_train_model.py
+++ learn_python/11.gensim_train_model.py
@@ -6,8 +6,9 @@ from gensim.models import word2vec
 import logging
 
 if __name__ == '__main__':
+
     if True:
-    # if not True:
+        # if not True:
         corpus_path = '/Users/axu/code/axuProject/Learn/learn_python/00.fixtures/03.word2vec/02.formateds/word2vec_segment.txt'
         models_path = '/Users/axu/code/axuProject/Learn/learn_python/00.fixtures/03.word2vec/03.models/trained_model.bin'
     else:
@@ -20,7 +21,7 @@ if __name__ == '__main__':
 
     sentences = word2vec.Text8Corpus(corpus_path)  # 加载语料
     # 训练skip-gram模型; 默认window=5
-    model = word2vec.Word2Vec(sentences, size=1500, window=5)
+    model = word2vec.Word2Vec(sentences, size=500, min_count=1)
     model.save_word2vec_format(models_path, binary=True)
 
     # result = model.most_similar(u'信用卡')
--- learn_python/13.gensim_use_model.py
+++ learn_python/13.gensim_use_model.py
@@ -8,17 +8,24 @@ import logging
 if __name__ == '__main__':
     logging.basicConfig(
         format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
     if True:
-    # if not True:
+        # if not True:
         models_path = '/Users/axu/code/axuProject/Learn/learn_python/00.fixtures/03.word2vec/03.models/trained_model.bin'
     else:
         models_path = '/Users/axu/code/axuProject/Learn/learn_python/00.fixtures/03.word2vec/03.models/non_stop_words_model.bin'
 
     model = word2vec.Word2Vec.load_word2vec_format(models_path, binary=True)
-    result = model.most_similar([u'客服', u'态度', u'不好'])
-    for r in result:
-        print r[0], r[1]
+    words = [u'信用卡', u'余额', u'不足', u'不好', u'北京', u'客服']
+    for word in words:
+        result = model.most_similar(word)
+        print '=' * 10 + word + '=' * 10
+        for r in result:
+            print r[0], r[1]
+
+        print '=' * 20
 
     # print(model.similarity(u"客服", u"坐席"))
 
     # print model.doesnt_match(u"早餐 服务 客服 坐席".split())
+    print('-EOF-')
