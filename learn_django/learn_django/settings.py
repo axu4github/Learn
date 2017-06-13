148c8317694ca6bb30030056216ce28c2396fbaf
anxu <axu.home@gmail.com>
2017-06-13 11:45:04 +0800

    修改数据库

--- learn_django/learn_django/settings.py
+++ learn_django/learn_django/settings.py
@@ -77,7 +77,10 @@ WSGI_APPLICATION = 'learn_django.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # 数据库路由设置（多数据库使用）
