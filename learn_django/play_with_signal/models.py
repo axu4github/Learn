764b0bd69fe9330d71aa424bce5ce7551be98a02
anxu <axu.home@gmail.com>
2017-06-14 10:11:09 +0800

    增强Signals post_save()功能

--- learn_django/play_with_signal/models.py
+++ learn_django/play_with_signal/models.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
 
 class Foo(models.Model):
 
     foo = models.CharField(max_length=20)
-    bar = models.CharField(max_length=30)
 
     def post_save(self):
-        print "this is post save function."
+        print "this is Class Foo post save function."
 
     def __unicode__(self):
-        return "{0}, {1}".format(self.foo, self.bar)
+        return "{0}".format(self.foo)
+
+
+class Bar(models.Model):
+
+    bar = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return "{0}".format(self.bar)
--- learn_django/play_with_signal/signals.py
+++ learn_django/play_with_signal/signals.py
@@ -1,11 +1,20 @@
-from .models import Foo
+# -*- coding: utf-8 -*-
+from .models import Foo, Bar
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+def common_post_save(sender, instance, **kwargs):
+    print "This is common post save function."
+
+
 @receiver(post_save, sender=Foo)  # 修饰器写法
+@receiver(post_save, sender=Bar)  # 修饰器写法
 def post_foo_save(sender, instance, **kwargs):
-    instance.post_save()
+    if hasattr(instance, "post_save"):
+        instance.post_save()
+    else:
+        common_post_save(sender, instance, **kwargs)
 
 # 初级写法
 # post_save.connect(post_foo_save, sender=Foo)
