93efa426d721534642c47006c0a873a731277574
anxu <axu.home@gmail.com>
2017-06-15 17:13:20 +0800

    添加租户

--- learn_django/learn_django/settings.py
+++ learn_django/learn_django/settings.py
@@ -34,20 +34,21 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'django.contrib.admin',
-    'django.contrib.auth',
+    'django.contrib.auth',  # 认证框架
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls',
     'play_with_signal.apps.PlayWithSignalConfig',
+    'play_with_auth.apps.PlayWithAuthConfig',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',  # 认证框架
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- learn_django/play_with_auth/models.py
+++ learn_django/play_with_auth/models.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Tenants(models.Model):
+    """租户表"""
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
