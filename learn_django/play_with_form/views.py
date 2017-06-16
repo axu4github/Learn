b8dfdb26bcf016cd0eae4684d2defc8156478f9f
anxu <axu.home@gmail.com>
2017-06-16 16:57:55 +0800

    完善基础测试

--- learn_django/play_with_form/views.py
+++ learn_django/play_with_form/views.py
@@ -1,43 +1,49 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from .forms import NameForm, ContactForm
 
 
 def name(request):
     # if this is a POST request we need to process the form data
-    if request.method == 'POST':
+    if request.method == "POST":
         # create a form instance and populate it with data from the request:
         form = NameForm(request.POST)
-        # check whether it's valid:
+        # check whether it"s valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            return HttpResponse("thanks!")
 
-    # if a GET (or any other method) we'll create a blank form
+    # if a GET (or any other method) we"ll create a blank form
     else:
         form = NameForm()
 
-    return render(request, 'forms/name.html', {'form': form})
+    return render(request, "forms/name.html", {"form": form})
 
 
 def contact(request):
     # if this is a POST request we need to process the form data
-    if request.method == 'POST':
+    if request.method == "POST":
         # create a form instance and populate it with data from the request:
         contact_form = ContactForm(request.POST)
-        # check whether it's valid:
+        # check whether it"s valid:
         if contact_form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            subject = contact_form.cleaned_data["subject"]
+            message = contact_form.cleaned_data["message"]
+            sender = contact_form.cleaned_data["sender"]
+            cc_myself = contact_form.cleaned_data["cc_myself"]
 
-    # if a GET (or any other method) we'll create a blank form
+            recipients = ["info@example.com"]
+            if cc_myself:
+                recipients.append(sender)
+
+            return HttpResponse("thanks")
+
+    # if a GET (or any other method) we"ll create a blank form
     else:
         contact_form = ContactForm()
 
-    return render(request, 'forms/contact.html', {'form': contact_form})
+    return render(request, "forms/contact.html", {"form": contact_form})
