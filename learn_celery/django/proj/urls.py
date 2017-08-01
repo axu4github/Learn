d61d5a286eda607837df2ed69bec9ca0701fdb4b
anxu <axu.home@gmail.com>
2017-08-02 00:25:27 +0800

    添加测试django-celery代码

---
+++ learn_celery/django/proj/urls.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.conf.urls import (
+    include, url, handler404, handler500,
+)
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # url(r"^$"),
+]
