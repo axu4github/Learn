304905dd96f417703ce9113d4635688580d208a4
axu4github <axu.home@gmail.com>
2017-10-28 17:03:25 +0800

    添加延迟方法

--- learn_python_run_time/tests.py
+++ learn_python_run_time/tests.py
@@ -3,33 +3,41 @@
 import unittest
 from datetime import datetime
 import time
+import sys
 
 
 class RunTimeTest(unittest.TestCase):
     """ 测试程序运行时间 """
 
+    def setUp(self):
+        self.sleep_time = 5
+
+    def delay_func(self):
+        """ 延迟方法 """
+        time.sleep(self.sleep_time)
+
     def test_process_total_run_time(self):
         """ 测试程序运行总时间 """
         s = datetime.now()
-        time.sleep(5)
+        self.delay_func()
         e = datetime.now()
-        print(e - s)
+        print("{} : {}".format(sys._getframe().f_code.co_name, e - s))
         pass
 
     def test_process_sys_run_time(self):
         """ 测试程序运行CPU时间 """
         s = time.clock()
-        time.sleep(5)
+        self.delay_func()
         e = time.clock()
-        print((e - s) * 1000)
+        print("{} : {}".format(sys._getframe().f_code.co_name, (e - s) * 1000))
         pass
 
     def test_prcess_total_run_time_by_time(self):
         """ 使用 time() 方法，测试程序运行总时间 """
         s = time.time()
-        time.sleep(5)
+        self.delay_func()
         e = time.time()
-        print(e - s)
+        print("{} : {}".format(sys._getframe().f_code.co_name, e - s))
         pass
 
 
