ea5113862083930fa0b3e4475a2af7ec2f82dca9
axu4github <axu.home@gmail.com>
2017-12-09 17:05:04 +0800

    修改代码格式使其符合PEP8标准

--- learn_spark_on_python/submit.py
+++ learn_spark_on_python/submit.py
@@ -1,43 +1,41 @@
 # -*- coding: UTF-8 -*-
 
-'''
+"""
 探索通过python如何提交spark任务
-'''
+"""
 
 import sys
 import os
 import time
-from multiprocessing import Process
 import random
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import HiveContext
 
-SPARK_MASTER = 'spark://axumatoMacBook-Pro.local:7077'
-SPARK_HOME = '/Users/axu/opt/spark-1.6.1-bin-hadoop2.6'
-PYSPARK_DIR = os.path.normpath(SPARK_HOME + '/python')
-PY4J_DIR = os.path.normpath(SPARK_HOME + '/python/lib/py4j-0.9-src.zip')
+SPARK_MASTER = "spark://axumatoMacBook-Pro.local:7077"
+SPARK_HOME = "/Users/axu/opt/spark-1.6.1-bin-hadoop2.6"
+PYSPARK_DIR = os.path.normpath(SPARK_HOME + "/python")
+PY4J_DIR = os.path.normpath(SPARK_HOME + "/python/lib/py4j-0.9-src.zip")
 
 sys.path.insert(0, PYSPARK_DIR)
 sys.path.insert(0, PY4J_DIR)
 
-if 'SPARK_HOME' not in os.environ:
-    os.environ['SPARK_HOME'] = SPARK_HOME
+if "SPARK_HOME" not in os.environ:
+    os.environ["SPARK_HOME"] = SPARK_HOME
 
-from pyspark import SparkContext, SparkConf
-from pyspark.sql import SQLContext, HiveContext
 
 SPARK_APP_NAME = "%s_%d" % (time.strftime(
-    '%Y-%m-%d_%H:%M:%S', time.localtime(time.time())), random.randint(0, 100))
+    "%Y-%m-%d_%H:%M:%S", time.localtime(time.time())), random.randint(0, 100))
 
 print SPARK_APP_NAME
 
-sql = 'select * from b'
+sql = "select * from b"
 
 SPARK_EXECUTOR_MEMORY = 1  # 单位：GB
 SPARK_EXECUTOR_CORES = 1  # 单位：核数
 
 SPARK_ENVS = {
-    'spark.executor.memory': "{executer_memory}g".format(executer_memory=SPARK_EXECUTOR_MEMORY),
-    'spark.executor.cores': SPARK_EXECUTOR_CORES,
-    # 'spark.executor.extraClassPath': "{base_dir}/lib/hbase2hive/jars/*".format(base_dir=BASE_DIR),
+    "spark.executor.memory": "{em}g".format(em=SPARK_EXECUTOR_MEMORY),
+    "spark.executor.cores": SPARK_EXECUTOR_CORES,
 }
 
 SPARK_CONF = SparkConf().setAppName(SPARK_APP_NAME).setMaster(SPARK_MASTER)
@@ -47,14 +45,12 @@ for k, v in SPARK_ENVS.items():
 SPARK_CONTEXT = SparkContext(conf=SPARK_CONF)
 SQL_CONTEXT = HiveContext(SPARK_CONTEXT)
 
+
 def spark_submit(SQL_CONTEXT):
     print SQL_CONTEXT.sql(sql).collect()
 
+
 for i in range(0, 10):
     spark_submit(SQL_CONTEXT)
-    # # 导入 SparkContext 和 SQLContext
-    # p = Process(target=spark_submit, args=SQL_CONTEXT)
-    # p.start()
-    # print p.pid
 
-print '-EOF-'
+print "-EOF-"
