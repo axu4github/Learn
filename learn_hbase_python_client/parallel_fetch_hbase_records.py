153e962f47fa57b7043f40cde9b8950b45d36668
anxu <axu.home@gmail.com>
2018-05-21 17:24:40 +0800

    添加创建Hbase记录方法

--- learn_hbase_python_client/parallel_fetch_hbase_records.py
+++ learn_hbase_python_client/parallel_fetch_hbase_records.py
@@ -3,6 +3,7 @@
 import happybase
 import threading
 import time
+import random
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -11,9 +12,9 @@ HBASE_HOST = "172.31.117.94"
 HBASE_TABLE = "smartv"
 
 
-def conn():
-    connection = happybase.Connection(HBASE_HOST)
-    table = connection.table(HBASE_TABLE)
+def conn(host=HBASE_HOST, table=HBASE_TABLE):
+    connection = happybase.Connection(host)
+    table = connection.table(table)
     return table
 
 
@@ -22,12 +23,20 @@ def fetch_hbase_record():
     print(table.row(b"B2C-IM-20180411-Lancome-1002.wav")["cf:area_of_job"])
 
 
+def put_record():
+    table = conn(table="parallel_put_record_table")
+    rowkey = str(random.randint(0, 100000 * 100000))
+    print(rowkey)
+    table.put(rowkey, {b"cf:test": b"a%s" % str(random.randint(0, 100000))})
+
+
 def main():
-    for j in range(100):
-        thread_number = 500
+    loop_number = 100
+    for j in range(loop_number):
+        thread_number = 5000
         threads = []
         for i in range(thread_number):
-            t = threading.Thread(target=fetch_hbase_record, name="fhr_thread")
+            t = threading.Thread(target=put_record, name="fhr_thread")
             t.start()
             threads.append(t)
 
